989da1b7b1a92c92d34ccd56970e349a087de49a
Nyuykisamo Lamfih <lamfihbilly@hotmail.com>
2021-08-13 17:26:10 +0100

    webcam capture component part one

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import './App.css';
-
+import Preview from "./Preview";
+import WebcamCapture from './WebcamCapture';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <Router>
+        <div className="app-body">
+          <Switch>
+            <Route  path="/preview">
+              <Preview />
+            </Route>
+            <Route path="/" exact>
+              <WebcamCapture />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
       <WebcamCapture />
     </div>
   );
--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,15 +1,36 @@
+import { RadioButtonChecked } from '@material-ui/icons';
+import React,{useRef, useCallback} from 'react'
+import Webcam from 'react-webcam';
+import {useDispatch} from "react-redux"
+import { setCameraImage } from './features/cameraSlice';
+
+
 const videoConstraiants = {
     width: 250,
     height: 400,
     facingNode: "user",
 };
 
-import React from 'react'
 
 function WebcamCapture() {
+    const webcamRef = useRef(null)
+    const dispatch = useDispatch()
+    const capture = useCallback(() => {
+        const imageSrc =webcamRef.current.getScreenshot();
+        dispatch(setCameraImage(imageSrc))
+    },[webcamRef])
     return (
-        <div>
-            
+        <div className="webcamCapture">
+            <Webcam
+            audio={false}
+            height={videoConstraiants.height}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={videoConstraiants.width}
+            videoConstraints={videoConstraiants}/>
+
+            <RadioButtonChecked className="webcamCapture-button"
+            onClick={capture} fontSize="large"/>
         </div>
     )
 }
